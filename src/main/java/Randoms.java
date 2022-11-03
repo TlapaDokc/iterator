7fc9cdda99457f51b28aac461fdeecb9d70c2168
Kuzin Dmitriy <SoraDnic@icloud.com>
2022-11-03 11:49:32 +0300

    initial commit

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private int min;
+    private int max;
+    private Random random = new Random();
+
+    public Randoms(int min, int max) {
+        this.max = max;
+        this.min = min;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return min + random.nextInt(max - min + 1);
+            }
+        };
+    }
+}
